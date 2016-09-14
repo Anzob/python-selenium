2baf1c582793510ba330a8bebef466c3c020c4cc
Anzob <abezrodnov@gmail.com>
2016-09-14 23:10:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,40 +4,132 @@ import os, sys, locale, _thread
 from selenium import webdriver
 from xvfbwrapper import Xvfb
 
-class PageWrapper():
-
-	def get_pageContentFunc(self, th_name, url, filename):
-		print("Thread %s start" % th_name)
-		
+class SetProxy():
+	def SetProxyFunc(self):
 		self.xvfb = Xvfb(width=1280, height=780, colordepth=16)
 		self.xvfb.start()
-		
-		print("xvfb_cmd %s" % self.xvfb.xvfb_cmd)
+
+		driver = webdriver.Firefox()
+		driver.get('http://myip.ru/')
+		#print(driver.title)
+		#element = driver.find_element_by_id('hostname')
+		element = driver.find_element_by_xpath('//table[@class="network-info"]')
+		print("old ip " + element.text)
+		driver.close()
+        #PROXY = "178.62.118.19:8118"
+		PROXY = "111.1.23.180:80"
+
+		webdriver.DesiredCapabilities.CHROME['proxy']={
+		    "httpProxy":PROXY,
+		    "ftpProxy":PROXY,
+		    "sslProxy":PROXY,
+		    "noProxy":None,
+		    "proxyType":"MANUAL",
+		    "autodetect":False
+		}
+		driver = webdriver.Chrome()
+		driver.get('http://myip.ru/')
+		#print(driver.title)
+		element = driver.find_element_by_xpath('//table[@class="network-info"]')
+		print("new ip " + element.text)
+		driver.close()
+		self.xvfb.stop()
+
+	def GetProxyList(self):
+		self.xvfb = Xvfb(width=1280, height=780, colordepth=16)
+		self.xvfb.start()
+
+		print("start parse body")
+		"""
+		PROXY = "178.62.118.19:8118"
+
+		webdriver.DesiredCapabilities.CHROME['proxy']={
+		    "httpProxy":PROXY,
+		    "ftpProxy":PROXY,
+		    "sslProxy":PROXY,
+		    "noProxy":None,
+		    "proxyType":"MANUAL",
+		    "autodetect":False
+		}
+		"""
+		driver = webdriver.Chrome()
+		driver.implicitly_wait(20)
+		driver.get('https://www.google.com/?gfe_rd=cr&gws_rd=ssl,cr&fg=1#safe=off&q=proxy+list')
+
+		fh = open('/home/alex/py/proxy_list.txt', "wb+")
+		##sbody = driver.find_element_by_xpath("/*")
+		#print(driver.page_source)
+		"""lst = list(driver.find_elements_by_xpath('/h3[@class="r"]'))"""
+		#lst = driver.find_elements_by_css_selector('h3.r')
+		lst = driver.find_elements_by_xpath("//h3[@class='r']/a")
+		print('len {0}', len(lst), type(lst))
+		for li in lst:
+			fh.write((li.get_attribute('href') + "\n").encode())
+			#print(li.get_attribute('href'))
+			##attrs = driver.execute_script('var items = {}; for (index = 0; index < arguments[0].attributes.length; ++index) { items[arguments[0].attributes[index].name] = arguments[0].attributes[index].value }; return items;', li)
+			##print(attrs)
+		fh.close()
+
+		driver.close()
+		self.xvfb.stop()
+		print("stop parse body")
+
+class PageWrapper():
+	def get_pageContentFunc(self, th_name, url, filename, filename2, useproxy):
+		print("Thread %s start" % th_name)
+
+		#self.xvfb = Xvfb(width=1280, height=780, colordepth=16)
+		#self.xvfb.start()
+		#print("xvfb_cmd %s" % self.xvfb.xvfb_cmd)
+
+		if useproxy:
+			#PROXY = "210.211.18.140:808"
+			PROXY = "122.96.59.106:83"
+			webdriver.DesiredCapabilities.CHROME['proxy']={
+				"httpProxy":PROXY,
+				"ftpProxy":PROXY,
+				"sslProxy":PROXY,
+				"noProxy":None,
+				"proxyType":"MANUAL",
+				"autodetect":False
+			}
+
 		driver = webdriver.Chrome()
 		driver.get(url)
+
+		fh = open(filename2, "wb+")
+		sbody = driver.find_element_by_xpath("//body")
+		fh.write((sbody.text).encode())
+		fh.close()
 
 		fh = open(filename, "wb+")
 		lst = list(driver.find_elements_by_xpath("//a[@class='link link_outer_yes path__item']"))
 		for ii in lst:
 			fh.write((ii.get_attribute('innerHTML') + "\n\n").encode())
 		fh.close()
+
 		driver.close()
-		
-		self.xvfb.stop()
-		#print("Xvfb end")
-		#pass
+
+		#self.xvfb.stop()
+
 		print("Thread %s end" % th_name)
 
-obj1 = PageWrapper()
-obj2 = PageWrapper()
+#obj1 = PageWrapper()
+#obj2 = PageWrapper()
+obj3 = SetProxy()
 
-#func1 = obj1.get_pageContentFunc(th_name = 'r1', url = 'https://yandex.ru/search/?text=грпш', filename = '/home/alex/py/result.txt')
-func1 = obj1.get_pageContentFunc
-func2 = obj2.get_pageContentFunc
-#func1(th_name = 'r1', url = 'https://yandex.ru/search/?text=грпш', filename = '/home/alex/py/result.txt')
+obj3.GetProxyList()
 
-#func2 = obj2.get_pageContentFunc('r1', 'https://yandex.ru/search/?text=грпш', '/home/alex/py/result.txt')
+#func1 = obj1.get_pageContentFunc
+#func2 = obj2.get_pageContentFunc
+"""
+xvfb = Xvfb(width=1280, height=780, colordepth=16)
+xvfb.start()
 
+func1(th_name='r1', url='https://yandex.ru/search/?text=грпш%20москва', filename='/home/alex/py/result.txt', filename2='/home/alex/py/result_tech.txt', useproxy=False)
+func1(th_name='r1', url='https://yandex.ru/search/?text=грпш%20москва', filename='/home/alex/py/result2.txt', filename2='/home/alex/py/result2_tech.txt', useproxy=True)
+
+xvfb.stop()"""
 """
 try:
    _thread.start_new_thread( func1, ("r1", "https://yandex.ru/search/?text=грпш", "/home/alex/py/result.txt") )
@@ -48,4 +140,3 @@ except:
 while 1:
    pass"""
 #get_pageContentFunc('r1', 'https://yandex.ru/search/?text=грпш', '/home/alex/py/result.txt')
-
